044cdaf0721bd1eb32876f5957eb5151cfff27fe
abreucan <79025012+abreucan@users.noreply.github.com>
2021-02-26 22:49:14 -0500

    Hello Damian, esta es solo la estructura de lo que podemos hacer con el main y los objetos de la clase Calculo

--- Main.java
+++ Main.java
@@ -9,3 +9,33 @@ public class   Main {
         System.out.println(Ca1);
     }
 }
+
+
+public class Main {
+        public static void main(String[] args) {
+            public int Type
+            System.out.println("Welcome to Ultimate Calculator/nSelect what kind of operation you are doing");
+            System.out.println("""
+                    Type 1: Add
+                    Type 2: Substract
+                    Type 3: Multiply
+                    Type 4: Divide""");
+            public int getType();
+             return Type;
+
+             switch (Type) {
+                 Case 1: {
+
+                 }
+                 Case 2: {
+
+                 }
+                 Case 3: {
+
+                 }
+                 Case 4: {
+
+                 }
+             }
+			       }
+			       }
